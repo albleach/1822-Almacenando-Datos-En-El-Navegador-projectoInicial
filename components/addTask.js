c8b9a2f6b551ff48b4248b6af9a59d2bd45e25d9
Beto <albleach@msn.com>
2022-10-08 12:21:18 -0600

    clase 03 Completada

--- components/addTask.js
+++ components/addTask.js
@@ -2,45 +2,53 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) => {
-  const list = document.querySelector("[data-list]");
-  const task = createTask(evento);
-  list.appendChild(task);
-};
-
-const createTask = (evento) => {
   evento.preventDefault();
-  const taskList = JSON.parse(localStorage.getItem('tasks')) ||[];
+
+  const list = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
   const calendar = document.querySelector("[data-form-date]");
+
   const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format("MM/DD/YYYY");
-  console.log(dateFormat);
-  const task = document.createElement("li");
-  task.classList.add("card");
+
+  if( value == "" || date == ""){
+    return;
+  }
+
   input.value = "";
-  //backticks
-  const taskContent = document.createElement("div");
+  calendar.value = "";
+
   const taskObject = {
     value,
     dateFormat,
   };
 
+  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
   taskList.push(taskObject);
-
-  //sessionStorage.setItem("task", JSON.stringify (taskObject));
   localStorage.setItem("tasks", JSON.stringify(taskList));
-  const titleTask = document.createElement("span");
-  titleTask.classList.add("task");
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
 
-  task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
+  const task = createTask(taskObject);
+  list.appendChild(task);
+};
+
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement("li");
+        task.classList.add("card");
+
+  const taskContent = document.createElement("div");
+
+  const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+
+  const dateElement = document.createElement("span");
+        dateElement.innerHTML = dateFormat;
+
+        task.appendChild(taskContent);
+        task.appendChild(dateElement);
+        task.appendChild(deleteIcon());
   return task;
 };
